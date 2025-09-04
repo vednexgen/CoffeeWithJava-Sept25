3793a6172f29d8ef99d593c59e0c71aa9154a7e4
Ved NexGen <ved.nexgen@gmail.com>
2025-09-04 09:45:21 +0530

    updated git ignore and deleted .class file

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,13 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-	
-	myMethod();
     }
-
-
-    public static void myMethod() {
-        System.out.println("Hello, Ved NexGen!");
-	}
 }
 

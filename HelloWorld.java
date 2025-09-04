e31dc2c9cc1cb2f62e2e6d2a896e2e7ee479bace
Ved NexGen <ved.nexgen@gmail.com>
2025-09-04 08:39:38 +0530

    hello world program

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+	
+	myMethod();
+    }
+
+
+    public static void myMethod() {
+        System.out.println("Hello, Ved NexGen!");
+	}
+}
+
